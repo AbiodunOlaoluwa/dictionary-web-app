734cf162814cfd4acd2e397f67f871a4531646bb
AbiodunOlaoluwa <mate.olaoluwa@gmail.com>
2024-05-13 20:31:23 +0100

    Made the App capable of remembering user preference for font type and theme

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 import pageIcon from "./images/iconoir_book.svg";
 import "./App.css";
@@ -9,12 +9,27 @@ const App = () => {
   const [theme, setTheme] = useState("light");
   const [font, setFont] = useState("sansSerif");
 
+  useEffect(() => {
+    setFont(localStorage.getItem('font'));
+  }, [])
+
+  useEffect(() => {
+    setTheme(localStorage.getItem('theme'));
+  }, [])
 
   const handleFontSelectChange = (event) => {
     setFont(event.target.value);
+    localStorage.setItem('font', event.target.value);
   }
   const handleThemeChangeClick = (event) => {
-    theme === "light" ? setTheme("dark") : setTheme("light")
+    if (theme === "light") {
+      setTheme("dark")
+      localStorage.setItem('theme', "dark")
+    }
+    else {
+      setTheme("light")
+      localStorage.setItem('theme', "light");
+    }
   }
 
   return (
@@ -36,9 +51,9 @@ const App = () => {
             </div>
             <div className="separationContainer"></div>
             <div className="themeSelectContainer">
-              <div className={`themeButtonContainer ${theme==="dark" ? "purpleBG" : ''}`} onClick={handleThemeChangeClick}>
+              <div className={`themeButtonContainer ${theme === "dark" ? "purpleBG" : ''}`} onClick={handleThemeChangeClick}>
                 <p className={`themeText`}>{theme === "light" ? "Light" : "Dark"}</p>
-                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22"><path fill="none" stroke={theme==='light' ? `#838383`: `#A445ED`} stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M1 10.449a10.544 10.544 0 0 0 19.993 4.686C11.544 15.135 6.858 10.448 6.858 1A10.545 10.545 0 0 0 1 10.449Z"/></svg>              </div>
+                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22"><path fill="none" stroke={theme === 'light' ? `#838383` : `#A445ED`} stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M1 10.449a10.544 10.544 0 0 0 19.993 4.686C11.544 15.135 6.858 10.448 6.858 1A10.545 10.545 0 0 0 1 10.449Z" /></svg>              </div>
             </div>
           </div>
         </div>
