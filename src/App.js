c79f9c84fdc5268231b518de4f82e448bae11f8d
AbiodunOlaoluwa <mate.olaoluwa@gmail.com>
2024-05-14 19:40:04 +0100

    Finished work on the search bar and all it's states, all that's left is to implement the actual search functionality for the app

--- src/App.js
+++ src/App.js
@@ -8,6 +8,15 @@ const App = () => {
 
   const [theme, setTheme] = useState("light");
   const [font, setFont] = useState("sansSerif");
+  const [error, setError] = useState(false);
+  const [searchWord, setSearchWord] = useState("");
+
+  useEffect(() => {
+    const searchInput = document.querySelector(".searchInput");
+    if (error === true) {
+      searchInput.blur();
+    }
+  }, [error])
 
   useEffect(() => {
     setFont(localStorage.getItem('font'));
@@ -32,8 +41,29 @@ const App = () => {
     }
   }
 
+  const handleSearchInputClick = () => {
+    setError(false)
+  }
+
+  const handleWordSearch = () => {
+    if (searchWord === "") {
+      setError(true);
+    }
+  }
+
+  const handleInputChange = (event) => {
+    setError(false);
+    setSearchWord(event.target.value)
+  }
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      handleWordSearch();
+    }
+  }
+
   return (
-    <div className={`App ${theme} ${font}`}>
+    <div className={`App ${theme} ${font}`} onClick={() => setError(false)}>
       <div className="mainDictionaryContainer">
         <div className="navBarContainer">
           <div className="bookIconContainer">
@@ -57,7 +87,13 @@ const App = () => {
             </div>
           </div>
         </div>
-        <div className="searchBarContainer"></div>
+        <div className="searchBar">
+          <div className={`searchBarContainer ${error ? "error" : ""}`}>
+            <input type="text" className={`searchInput ${error ? "errorPlaceholder" : ""}`} placeholder='Search for any word...' value={searchWord} onChange={handleInputChange} onKeyDown={handleKeyDown} onClick={handleSearchInputClick} />
+            <svg className="searchIcon" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18"><path fill="none" stroke={error ? "#FF5252" : "#A445ED"} stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m12.663 12.663 3.887 3.887M1 7.664a6.665 6.665 0 1 0 13.33 0 6.665 6.665 0 0 0-13.33 0Z"/></svg>
+          </div>
+          <p className={`errorMessage ${error ? "" : "noDisplay"} ${error ? "errorFont" : ""}`}>Whoops, can't be empty...</p>
+        </div>
         <div className="resultBody"></div>
       </div>
     </div>
